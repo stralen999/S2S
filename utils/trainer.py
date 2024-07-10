9fe65c8a897462f8895fe25859c713a7e9486afa
stralen999 <71458993+stralen999@users.noreply.github.com>
2024-07-10 16:16:54 +0100

    Add files via upload

--- utils/trainer.py
+++ utils/trainer.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
 import numpy as np
+from io import StringIO
 
 class Trainer():
 	def __init__(self, model, train_data, val_data, criterion, optimizer, max_epochs, device, path, patience, mask,
@@ -45,19 +46,23 @@ class Trainer():
 	def train(self, train_losses, train_set):
 		train_loss = self.model.train()
 		epoch_train_loss = 0.0
-		for i, (x, y) in enumerate(train_set):
+		for i, (x, y, x_day,y_day) in enumerate(train_set):
+			#print("i %s x %s y %s of %s y_day %s" % (i,x.shape,y.shape,len(train_set),y_day) )
 			x,y = x.to(self.device), y.to(self.device)
 			self.optimizer.zero_grad()
 			if (self.lilw):
-				output = self.model(x, original_x = x)
+				output = self.model(x, original_x = x)[:,:,-1:,:,:]
 			else:
-				output = self.model(x)
+				output = self.model(x)[:,:,-1:,:,:]
 			#Disregard undefined pixels
 			output = output*self.mask_land
-			if (self.cut_output and not self.recurrent_model):
-				loss = self.criterion(output[:,:,0,:,:], y[:,:,0,:,:])
-			else:
-				loss = self.criterion(output, y)
+			print("______________________________________________________output[:,:,0,:,:]",output[:,:,0,:,:]," y[:,:,0,:,:]", y[:,:,0,:,:])
+			print("_________output[:,:,0,:,:] y[:,:,0,:,:].shape",output[:,:,0,:,:].shape,y[:,:,0,:,:].shape)
+			#if ( not self.recurrent_model):
+			#if (self.cut_output and not self.recurrent_model):
+			loss = self.criterion(output[:,:,0,:,:], y[:,:,0,:,:])
+			#else:
+			#	loss = self.criterion(output, y)
 			loss.backward()
 			self.optimizer.step()
 			epoch_train_loss += loss.detach().item()
@@ -68,9 +73,11 @@ class Trainer():
 		epoch_val_loss = 0.0
 		self.model.eval()
 		with torch.no_grad():
-			for i, (x, y) in enumerate(self.val_data):
+			for i, (x, y,x_day, y_day) in enumerate(self.val_data):
+				print("____________________________________________________i %s x %s y %s of %s y_day %s" % (i,x.shape,y.shape,len(self.val_data),y_day) )
 				x,y = x.to(self.device), y.to(self.device)
 				output = self.model(x)
+				print("____________________x y out",x,y,output)
 				#Disregard undefined pixels
 				output = output * self.mask_land
 				loss = self.criterion(output, y)
@@ -123,7 +130,7 @@ class EarlyStop:
 		print ('=> Saving a new best')
 
 class Tester():
-	def __init__(self, model, optimizer, criterion, test_data, device, model_name, mask, online_learning_epochs = 5):
+	def __init__(self, model, optimizer, criterion, test_data, device, model_name, mask,forecast_date, online_learning_epochs = 5):
 		self.model = model
 		self.model_name = model_name
 		self.optimizer = optimizer
@@ -132,30 +139,89 @@ class Tester():
 		self.device = device
 		self.mask_land = mask.to(self.device)
 		self.online_learning_epochs = online_learning_epochs
+		self.forecast_date = forecast_date
 
 	def load_and_test(self, path):
 		self.load_model(path)
 		return self.test_model()
 
+	def write_asc(self,path,grid):
+		f = StringIO()
+		#x = np.array(( -9999, 1.345, -9999, 3.21, 0.13, -9999), dtype=float)
+		grid[grid == 0] = -999.250
+		np.savetxt(f, grid, fmt='%.3f')
+		f.seek(0)
+		fs = f.read().replace('-999.250', '-999.250', -1)
+		f.close()
+		f = open(path, 'w')
+		f.write("ncols	" + str(141) + "\n")
+		f.write("nrows	" + str(288) + "\n")
+		f.write("xllcorner	" + str(-120191.398) + "\n")
+		f.write("yllcorner	" + str(-301404.813) + "\n")
+		f.write("cellsize	2000.000\n")
+		f.write("NODATA_value	-999.250\n")
+		f.write(fs)
+		f.close()
+
 	def test_model(self):
 		batch_rmse_loss = 0.0
 		batch_mae_loss = 0.0
 		batch_r2 = 0.0
+		curr = 0
 		preds = []
 		preds_border = []
-		for i, (x, x_border, y, y_border) in enumerate(self.test_data):
+		print(enumerate(self.test_data))
+		#no_days = len(enumerate(self.test_data))
+		no_days=0
+		for i, (x, x_border, y, y_border, x_day,y_day) in enumerate(self.test_data):
+			no_days += 1
+
+		print("no_days",no_days)
+		
+		pred_matrix = np.empty((no_days,288,141))
+		for i, (x, x_border, y, y_border, x_day,y_day) in enumerate(self.test_data):
 			x,x_border,y,y_border = x.to(self.device), x_border.to(self.device), y.to(self.device), y_border.to(self.device)
 			self.model.eval()
 			with torch.no_grad():
-				output = self.model(x)
+				output = self.model(x) # introduces X, returns output
+				print("TESTS2S i %s x %s y %s of %s y_day %s y_value r46c50 %s output r46c50 %s" % (i,x.shape,y.shape,len(self.test_data),y_day,y[0,0,0,46,50].cpu().numpy(),output[0,0,0,46,50].cpu().numpy()) )
+				
+				y_single_loc = y[0,0,0,46,50].cpu().numpy()
+				out_single_loc = output[0,0,0,46,50].cpu().numpy()
+				term = (out_single_loc-y_single_loc)*(out_single_loc-y_single_loc)
+				curr += term
+				print("i %s out_single_loc %s y_single_loc %s loss %s " % (i,out_single_loc,y_single_loc,term) )
+
 				#Disregard undefined pixels
-				output = output * self.mask_land
-				loss_rmse = self.criterion(output, y)
+			
+				print("os.getcwd()",os.getcwd(),"y_day[0][0",y_day[0][0],"self.forecast_date",self.forecast_date)
+				if y_day[0][0] == self.forecast_date:
+					print("write forecast")
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_7thday.asc', output[0,0,6,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_6thday.asc', output[0,0,5,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_5thday.asc', output[0,0,4,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_4thday.asc', output[0,0,3,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_3rdday.asc', output[0,0,2,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_2ndday.asc', output[0,0,1,:,:].cpu().numpy())
+					self.write_asc(os.getcwd()+'/prediction_s2s__medianmodel_'+y_day[0][0]+'_1stday.asc', output[0,0,0,:,:].cpu().numpy())
+
+				output = output[:,:,-1,:,:] * self.mask_land
+
+				print("output.shape %s self.mask_land %s y %s" % (output.shape, self.mask_land.shape, y.shape) )
+				print("y_day[0][0] %s self.mask_land %s " % (y_day[0][0], 1) )
+				#self.write_asc('prediction_s2s__medianmodel_'+y_day[0][0]+'.asc', output[0,0,0,:,:].cpu().numpy())
+				#self.write_asc('actual_s2s__medianmodel_'+y_day[0][0]+'.asc', y[0,0,0,:,:].cpu().numpy())
+				pred_matrix[i,:,:] = output[0,0,0,:,:].cpu().numpy()
+
+				loss_rmse = self.criterion(output, y) # compares output with y, then day of y is whats concerns
 				loss_mae = F.l1_loss(output, y)
 				r2,ar2 = self.report_r2(output.cpu(), y.cpu())
 				for j in range(output.shape[0]):
 					preds.append(output[j,0,0,:,:].cpu().numpy())
-				batch_rmse_loss += loss_rmse.detach().item()
+
+				loss_rmse_detach = loss_rmse.detach().item()
+				print("batch_rmse_loss %s loss_rmse_detach %s" % (batch_rmse_loss,loss_rmse_detach) )
+				batch_rmse_loss += loss_rmse_detach
 				batch_mae_loss += loss_mae.detach().item()
 				batch_r2 += ar2
 			for e in range(self.online_learning_epochs):
@@ -164,7 +230,18 @@ class Tester():
 		mae_loss = batch_mae_loss/len(self.test_data)
 		r2_metric = batch_r2/len(self.test_data)
 		self.save_prediction(preds, "region_3")
+
+		rmse_loss_single_loc = np.sqrt(curr/len(self.test_data))
+		print("rmse_loss_single_loc %s " % (rmse_loss_single_loc ) )
 		return rmse_loss, mae_loss, r2_metric
+
+# TESTS2S i 0 x torch.Size([1, 1, 7, 288, 141]) y torch.Size([1, 1, 1, 288, 141]) of 93 y_day [('01-Sep-2020',)]
+# output torch.Size([1, 1, 1, 288, 141]) self.mask_land torch.Size([1, 1, 1, 288, 141]) y torch.Size([1, 1, 1, 288, 141])
+# batch_rmse_loss 0.0 loss_rmse_detach 744.0697631835938
+# TESTS2S i 1 x torch.Size([1, 1, 7, 288, 141]) y torch.Size([1, 1, 1, 288, 141]) of 93 y_day [('02-Sep-2020',)]
+# output torch.Size([1, 1, 1, 288, 141]) self.mask_land torch.Size([1, 1, 1, 288, 141]) y torch.Size([1, 1, 1, 288, 141])
+# batch_rmse_loss 744.0697631835938 loss_rmse_detach 559.979736328125
+
 
 	def online_learning(self, x, y):
 		self.model.train()
